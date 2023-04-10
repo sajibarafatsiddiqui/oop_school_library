8a3a480ae4c777fadeceb5d70f9c0a29744cccc5
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-04-11 04:56:31 +0600

    Added Person, Student and Teacher classes

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require './person'
+# added the class Teacher
+class Teacher < Person
+  def initialize(age:, specialization:, name: 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
