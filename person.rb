0d505cd28b169c2344c52f09ce23895f735ff0ed
Sajib Arafat Siddiqui <sajib.arafat@nrbglobalbank.com>
2023-04-11 15:36:19 +0600

    Corrected the constructor parameters

--- person.rb
+++ person.rb
@@ -5,7 +5,7 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1...1000)
     @name = name
     @age = age
@@ -14,7 +14,7 @@ class Person
 
   private
 
-  def of_age
+  def is_of_age?
     return true if @age >= 18
 
 
@@ -22,7 +22,7 @@ class Person
   end
 
   def can_use_services?
-    return true if of_age? || @parent_permission
+    return true if is_of_age? || @parent_permission
 
 
     false
